e7722b3bb14db234431beb8d8ab5979317254e09
VaultDweller13 <grognak13@gmail.com>
2023-03-18 20:58:29 +0400

    feat: add card styling

--- components/src/components/Card.tsx
+++ components/src/components/Card.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Card.css';
+import icon from '../assets/img/metacritic-icon.svg';
 
 type CardProps = {
   title: string;
   img: string;
   price: string;
+  score: string;
 };
 
 export default class Card extends React.Component<CardProps> {
@@ -13,6 +16,10 @@ export default class Card extends React.Component<CardProps> {
         <img className="card_image" src={this.props.img} />
         <h3 className="card_title">{this.props.title}</h3>
         <p className="card_price">{this.props.price}</p>
+        <div className="card_score">
+          <img className="score_icon" src={icon} />
+          <span className="score_value">{this.props.score}</span>
+        </div>
       </div>
     );
   }
--- components/src/components/CardContainer.tsx
+++ components/src/components/CardContainer.tsx
@@ -5,7 +5,13 @@ import Card from './Card';
 export default class CardContainer extends React.Component {
   render() {
     const cards = games.map((game) => (
-      <Card key={game.id} title={game.title} img={`../img/cards/${game.img}`} price={game.price} />
+      <Card
+        key={game.id}
+        title={game.title}
+        img={`../img/cards/${game.img}`}
+        price={game.price}
+        score={game.score}
+      />
     ));
 
     return <section className="cards-container">{cards}</section>;
