52e91fa34b57409641bd7df6c43884a81dde9a5e
VaultDweller13 <grognak13@gmail.com>
2023-03-31 14:30:50 +0400

    refactor: rewrite App component as functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -6,18 +5,16 @@ import NotFound from './pages/NotFound';
 import About from './pages/About';
 import FormPage from './pages/FormPage';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/form" element={<FormPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="/*" element={<Navigate to={'/404'} />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/form" element={<FormPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/*" element={<Navigate to={'/404'} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
