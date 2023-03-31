5697ddde4e72eba6879c3894972c689f7fe1c3af
VaultDweller13 <grognak13@gmail.com>
2023-03-31 15:56:49 +0400

    refactor: rewrite Card and SearchBar as functional components

--- src/components/card/CardButtons.tsx
+++ src/components/card/CardButtons.tsx
@@ -1,32 +1,26 @@
-import React from 'react';
+import { useState } from 'react';
 
 import './CardButtons.css';
 import fav from '../../assets/img/favorite_empty.svg';
 import favFilled from '../../assets/img/favorite_filled.svg';
 import type { CardButtonsProps } from '../../utils/types';
 
-export default class CardButtons extends React.Component<CardButtonsProps> {
-  state = {
-    isFavorite: false,
+export default function CardButtons(props: CardButtonsProps) {
+  const [isFavorite, setFavorite] = useState(false);
+  const path = '../img/logo/';
+  const handleClick = () => {
+    setFavorite(!isFavorite);
   };
 
-  handleClick = () => {
-    this.setState({ isFavorite: !this.state.isFavorite });
-  };
-
-  render() {
-    const path = '../img/logo/';
-
-    return (
-      <div className="card_buttons">
-        <button type="button" className="button_main">
-          Add to Cart
-        </button>
-        <button type="button" className="button_wishlist" onClick={this.handleClick}>
-          <img className="wishlist-icon" src={this.state.isFavorite ? favFilled : fav} />
-        </button>
-        <img src={`${path}${this.props.platform}.svg`} className="card_logo" />
-      </div>
-    );
-  }
+  return (
+    <div className="card_buttons">
+      <button type="button" className="button_main">
+        Add to Cart
+      </button>
+      <button type="button" className="button_wishlist" onClick={handleClick}>
+        <img className="wishlist-icon" src={isFavorite ? favFilled : fav} />
+      </button>
+      <img src={`${path}${props.platform}.svg`} className="card_logo" />
+    </div>
+  );
 }
--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,39 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+
 import './SearchBar.css';
 import icon from '../../assets/img/search.svg';
 
-export default class SearchBar extends React.Component {
-  state = {
-    searchInput: localStorage.getItem('searchInput') || '',
-  };
-
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+export default function SearchBar() {
+  const [input, setInput] = useState(localStorage.getItem('searchInput') || '');
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!(event.target instanceof HTMLInputElement)) return;
 
-    const { value } = event.target;
-    this.setState({ searchInput: value });
+    setInput(event.target.value);
   };
 
-  componentDidUpdate(): void {
-    localStorage.setItem('searchInput', this.state.searchInput);
-  }
+  useEffect(() => {
+    localStorage.setItem('searchInput', input);
+  });
 
-  render() {
-    return (
-      <div className="search-bar">
-        <form className="search_form">
-          <input
-            type="search"
-            className="search_input"
-            placeholder="Search"
-            onChange={this.handleChange}
-            value={this.state.searchInput}
-          />
-          <button type="button" className="search_button">
-            <img src={icon} alt="Search icon" className="search_icon" />
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="search-bar">
+      <form className="search_form">
+        <input
+          type="search"
+          className="search_input"
+          placeholder="Search"
+          onChange={handleChange}
+          value={input}
+        />
+        <button type="button" className="search_button">
+          <img src={icon} alt="Search icon" className="search_icon" />
+        </button>
+      </form>
+    </div>
+  );
 }
