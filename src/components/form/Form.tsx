61061740bc592851608bf946b6b531460df1f2a4
VaultDweller13 <grognak13@gmail.com>
2023-04-02 20:20:28 +0400

    feat: migrate form logic and validation to react-hook-form library

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 import './Form.css';
 import Select from './Select';
-import LabeledInput from './LabeledInput';
 import genres from '../../assets/data/genres.json';
-import CustomCard from '../card/Card';
-import { textInputIsValid, dateInputIsValid, genresInputIsValid } from '../../utils/validation';
+import Card from '../card/Card';
 import type { CardProps } from '../../utils/types';
 
 type Inputs = {
@@ -20,126 +18,33 @@ type Inputs = {
 
 export default function Form() {
   const form = React.createRef<HTMLFormElement>();
-  const titleInput = React.createRef<HTMLInputElement>();
-  const dateInput = React.createRef<HTMLInputElement>();
-  const platformInput = React.createRef<HTMLSelectElement>();
-  const genresInput = genres.map(() => React.createRef<HTMLInputElement>());
-  const digitalInput = React.createRef<HTMLInputElement>();
-  const physicalInput = React.createRef<HTMLInputElement>();
-  const imageInput = React.createRef<HTMLInputElement>();
-
   const [cards, setCards] = useState<JSX.Element[]>([]);
-  const [validationMessages, setValidationMessages] = useState({
-    title: '',
-    date: '',
-    platform: '',
-    genres: '',
-    format: '',
-    image: '',
-  });
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<Inputs>();
 
   const onSubmit = handleSubmit((data) => {
-    console.log(data);
+    addCard(data);
+    reset();
+    alert('Game added!');
   });
 
-  // const onSubmit = (event: React.MouseEvent) => {
-  //   event.preventDefault();
-  //   resetValidationMessages();
-  //   let isValid = true;
-
-  //   if (!textInputIsValid(titleInput.current)) {
-  //     setValidationMessage('title', "You must provide the game's title");
-  //     isValid = false;
-  //   }
-
-  //   if (!dateInputIsValid(dateInput.current)) {
-  //     setValidationMessage('date', 'The first game ever was made in the 1950');
-  //     isValid = false;
-  //   }
-
-  //   if (!textInputIsValid(platformInput.current)) {
-  //     setValidationMessage('platform', 'Please choose a platform');
-  //     isValid = false;
-  //   }
-
-  //   if (!platformInpitIsValid(genresInput)) {
-  //     setValidationMessage('genres', 'Please choose 1 to 3 genres');
-  //     isValid = false;
-  //   }
-
-  //   if (!(digitalInput.current?.checked || physicalInput.current?.checked)) {
-  //     setValidationMessage('format', "Please specify the game's format");
-  //     isValid = false;
-  //   }
-
-  //   if (!imageInput.current?.files?.length) {
-  //     setValidationMessage('image', 'Please provide a cover image');
-  //     isValid = false;
-  //   }
-
-  //   if (isValid) {
-  //     addCard();
-  //     form.current?.reset();
-  //     alert('Game added!');
-  //   }
-  // };
-
-  const setValidationMessage = (
-    field: 'title' | 'date' | 'platform' | 'genres' | 'format' | 'image',
-    message: string
-  ) => {
-    setValidationMessages((prevState) => ({
-      ...prevState,
-      [field]: message,
-    }));
-  };
-
-  const resetValidationMessages = () => {
-    setValidationMessages({
-      title: '',
-      date: '',
-      platform: '',
-      genres: '',
-      format: '',
-      image: '',
-    });
-  };
-
-  const getCardProps = () => {
-    if (!titleInput.current) return;
-    if (!dateInput.current) return;
-    if (!platformInput.current) return;
-    if (!imageInput.current) return;
-    if (!imageInput.current.files) return;
-
-    const genres = genresInput
-      .map((ref) => {
-        if (ref.current && ref.current.checked) return ref.current.value;
-      })
-      .filter((genre) => genre) as string[];
-
-    const imgSrc = URL.createObjectURL(imageInput.current?.files[0]);
-
-    return {
-      title: titleInput.current.value,
-      date: dateInput.current.value,
-      platform: platformInput.current.value,
-      genres: genres,
-      format: digitalInput.current?.checked ? ('digital' as const) : ('physical' as const),
-      img: imgSrc,
+  const addCard = (data: Inputs) => {
+    const props = {
+      title: data.title,
+      date: data.date,
+      platform: data.platform,
+      genres: data.genres,
+      format: data.format,
+      img: URL.createObjectURL(data.image[0]),
       price: '59.99',
       score: null,
-    };
-  };
+    } as CardProps;
 
-  const addCard = () => {
-    const props = getCardProps() as CardProps;
-    setCards((prevState) => [...prevState, <CustomCard key={prevState.length + 1} {...props} />]);
+    setCards((prevState) => [...prevState, <Card key={prevState.length + 1} {...props} />]);
   };
 
   return (
@@ -166,7 +71,7 @@ export default function Form() {
               type="date"
               id="form_game-date"
               className="form_input input_date"
-              {...register('date', { required: true, validate: dateInputIsValid })}
+              {...register('date', { validate: dateInputIsValid })}
             />
             <p className="validation-message">{errors.date?.message}</p>
           </li>
@@ -177,46 +82,36 @@ export default function Form() {
           <li className="form_item">
             <h3 className="form_label">Genres:</h3>
             <div className="form_checkbox-container">
-              {genres.map((genre, index) => (
-                <LabeledInput
-                  register={register}
-                  key={genre.id}
-                  type="checkbox"
-                  name="genre"
-                  id={genre.genre}
-                  label={genre.label}
-                  value={genre.label}
-                  forwardedRef={genresInput[index]}
-                />
+              {genres.map((genre) => (
+                <label key={genre.id} className="labeledInput_label">
+                  <input
+                    type="checkbox"
+                    id={genre.genre}
+                    value={genre.label}
+                    {...register('genres', { required: 'Please specify at least 1 genre' })}
+                  />
+                  {genre.label}
+                </label>
               ))}
             </div>
-            <p className="validation-message">
-              {genresInputIsValid(
-                genresInput
-                  .map((input) => input.current?.value)
-                  .filter((value) => value) as string[]
-              )}
-            </p>
+            <p className="validation-message">{errors.genres?.message}</p>
           </li>
           <li className="form_item">
             <h3 className="form_label">Format:</h3>
             <div className="form_radio-container">
-              <LabeledInput
-                register={register}
-                name="format"
-                type="radio"
-                id="digital"
-                label="Digital"
-                value="digital"
-              />
-              <LabeledInput
-                register={register}
-                name="format"
-                type="radio"
-                id="physical"
-                label="Physical"
-                value="physical"
-              />
+              <label className="labeledInput_label">
+                <input
+                  type="radio"
+                  id="digital"
+                  value="digital"
+                  {...register('format', { required: "Please specify the game's format" })}
+                />
+                Digital
+              </label>
+              <label className="labeledInput_label">
+                <input type="radio" id="physical" value="physical" {...register('format')} />
+                Physical
+              </label>
             </div>
             <p className="validation-message">{errors.format?.message}</p>
           </li>
@@ -229,7 +124,7 @@ export default function Form() {
               id="form_game-cover"
               accept="image/png, image/jpeg"
               className="form_input input_file"
-              {...register('image', { required: true })}
+              {...register('image', { required: 'Please provide a cover image' })}
             />
             <p className="validation-message">{errors.image?.message}</p>
           </li>
@@ -241,4 +136,12 @@ export default function Form() {
       {cards}
     </main>
   );
+}
+
+function dateInputIsValid(value: string | null) {
+  const message = 'The first game ever was made in the 1950';
+
+  if (!value) return message;
+
+  return new Date('1950') < new Date(value) || message;
 }
