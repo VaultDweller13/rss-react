18c98dd5877415fbfab096543bc0be11d85db3b8
VaultDweller13 <grognak13@gmail.com>
2023-04-09 19:31:29 +0400

    feat: add close button to modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,4 +1,5 @@
 import './Modal.css';
+import icon_close from '../../assets/img/close.svg';
 
 type ModalProps = {
   active: boolean;
@@ -13,6 +14,12 @@ export default function Modal(props: ModalProps) {
         className={props.active ? 'modal_content active' : 'modal_content'}
         onClick={(e) => e.stopPropagation()}
       >
+        <img
+          className="modal_close"
+          src={icon_close}
+          alt="Close modal button"
+          onClick={() => props.setActive(false)}
+        />
         {props.child}
       </div>
     </div>
