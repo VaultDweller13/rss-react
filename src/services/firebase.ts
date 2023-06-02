0e44a51036c85d89106ac1fb2047382f130c91d8
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2023-06-02 14:44:03 +0400

    feat: set up firebase auth functions

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,10 @@
 import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_KEY,
@@ -9,4 +15,29 @@ const firebaseConfig = {
   appId: '1:672316513345:web:fa1d42937bcba7a7b9fac0',
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export const createUser = async (email: string, password: string) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const logIn = async (email: string, password: string) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const logOut = async () => {
+  try {
+    signOut(auth);
+  } catch (error) {
+    console.error(error);
+  }
+};
