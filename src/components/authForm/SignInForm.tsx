6bc51b18ccfdc7119c707a3bde264a5d1d352a51
VaultDweller13 <grognak13@gmail.com>
2023-06-06 01:05:48 +0400

    feat: bind createUser and logIn functions to buttons

--- src/components/authForm/SignInForm.tsx
+++ src/components/authForm/SignInForm.tsx
@@ -1,11 +1,25 @@
+import { useCallback, useRef } from 'react';
 import { TextField } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
 
+import { logIn } from '../../services/firebase';
 import styles from './Form.module.css';
 
 export default function SignInForm() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleSubmit = useCallback(async () => {
+    const email = emailRef.current;
+    const password = passwordRef.current;
+
+    if (email && password) {
+      await logIn(email, password);
+    }
+  }, [emailRef, passwordRef]);
+
   return (
     <>
       <form className={styles.form}>
@@ -16,6 +30,7 @@ export default function SignInForm() {
           name="email"
           id="auth-email"
           autoComplete="current-email"
+          ref={emailRef}
         />
         <TextField
           type="password"
@@ -24,8 +39,11 @@ export default function SignInForm() {
           name="password"
           id="auth-password"
           autoComplete="current-password"
+          ref={passwordRef}
         />
-        <Button variant="contained">Sign in</Button>
+        <Button variant="contained" onClick={handleSubmit}>
+          Sign in
+        </Button>
         <Link component={RouterLink} to="/signUp" className={styles.link}>
           Create account
         </Link>
--- src/components/authForm/SignUpForm.tsx
+++ src/components/authForm/SignUpForm.tsx
@@ -1,11 +1,25 @@
+import { useRef } from 'react';
 import { TextField } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
 
+import { createUser } from '../../services/firebase';
 import styles from './Form.module.css';
 
 export default function SignUpForm() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async () => {
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (email && password) {
+      await createUser(email, password);
+    }
+  };
+
   return (
     <>
       <form className={styles.form}>
@@ -16,6 +30,7 @@ export default function SignUpForm() {
           name="email"
           id="register-email"
           autoComplete="current-email"
+          inputRef={emailRef}
         />
         <TextField
           type="password"
@@ -24,8 +39,11 @@ export default function SignUpForm() {
           name="password"
           id="register-password"
           autoComplete="new-password"
+          inputRef={passwordRef}
         />
-        <Button variant="contained">Sign up</Button>
+        <Button variant="contained" onClick={handleSubmit}>
+          Sign up
+        </Button>
         <Link component={RouterLink} to="/signIn" className={styles.link}>
           Sign in
         </Link>
